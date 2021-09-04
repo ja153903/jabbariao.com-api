751c23fc4cec0372156a51375331c0670b219e4d
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-04 18:27:30 -0400

    Adding new config

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -4,11 +4,26 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { customScalarResolvers } from "./scalarTypes";
 
 import { typeDefs as postTypeDefs, resolvers as postResolvers } from "../posts";
+import { cors } from "../config";
 
 const typeDefs = mergeTypeDefs([postTypeDefs]);
 const resolvers = mergeResolvers([postResolvers, customScalarResolvers]);
 
-export default new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+async function startApolloServer(app, httpServer, port) {
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors });
+
+  await new Promise((resolve) => httpServer.listen({ port }, resolve));
+
+  console.log(
+    `GraphQL server available at localhost:${port}${apolloServer.graphqlPath}`,
+  );
+}
+
+export { startApolloServer };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import Express from "express";
-import cors from "cors";
+import http from "http";
 
-import apolloServer from "./graphql";
+import { startApolloServer } from "./graphql";
 
 const app = Express();
+const httpServer = http.createServer(app);
 const port = process.env.PORT || 5050;
 
-app.use(cors());
-app.use(Express.json());
-app.use(Express.urlencoded());
-
-async function startApolloServer() {
-  await apolloServer.start();
-
-  apolloServer.applyMiddleware({ app });
-
-  app.listen(port, () => {
-    console.log(
-      `GraphQL server available at localhost:${port}${apolloServer.graphqlPath}`,
-    );
-  });
-}
-
-startApolloServer();
+startApolloServer(app, httpServer, port);
